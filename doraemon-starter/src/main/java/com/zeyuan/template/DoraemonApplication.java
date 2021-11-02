a8783650e656b3eacfd47113f3b1de756b98fdf9
liangwei <372856634@qq.com>
2021-11-02 22:05:13 +0800

    初始化

---
+++ doraemon-starter/src/main/java/com/zeyuan/template/DoraemonApplication.java
@@ -0,0 +1,13 @@
+package com.zeyuan.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoraemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoraemonApplication.class, args);
+	}
+
+}

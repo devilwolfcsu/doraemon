3fd631be433c26378a6d8953e792708755ed2337
liangwei <372856634@qq.com>
2021-11-04 00:03:02 +0800

    调整module结构

---
+++ doraemon-starter/src/test/java/com/zeyuan/template/DoraemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeyuan.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoraemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
